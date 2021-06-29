dc7115fcb41ee6ce725abf8ae6b4df041f67bfc0
Rajat Kulkarni <rajat.kulkarni48@gmail.com>
2021-06-29 21:17:03 +0530

    First commit

--- predict.py
+++ predict.py
@@ -1,41 +1,72 @@
 from torchvision import datasets, models, transforms
 import torch
+import cv2
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from streamlit_webrtc import ClientSettings
 import torch.nn.functional as F
 import streamlit as st 
-from pretrained_model import vgg16model
-loader = transforms.Compose([
-    transforms.Resize(256),
-    transforms.ColorJitter(),
-    transforms.RandomCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.Resize(128),
+from pretrained_model import resnet
+transform= transforms.Compose([
+    transforms.Resize(224),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 from PIL import Image
 
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": True, "audio": False},
+    )
 
-checkpoint = torch.load('saved_model.pt')
-vgg16model.load_state_dict(checkpoint['model_state_dict'])
+checkpoint = torch.load('resnet18_final_model.pt',map_location=torch.device("cpu"))
+resnet.load_state_dict(checkpoint['model_state_dict'])
 
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-  
-    image_tensor = loader(image).float()
-    image_tensor = image_tensor.unsqueeze(0)
-    pred=vgg16model(image_tensor)
-    prob=F.softmax(pred,dim=1)
+resnet.eval()
+class VideoTransformer(VideoTransformerBase):
     
-    if(prob[0][0]>0.50):
-        st.write(" No Mask detected")
-    else:    
-        st.write("Mask Detected")
+        def __init__(self):
+            self.canvas = None
+            self.x1, self.y1 = 0, 0
 
-    st.image(image)    
+        def transform(self, frame):
+            frame= frame.to_ndarray(format="bgr24")
+            #frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            image=Image.fromarray(frame)
+            image=transform(image)
+            image=image.unsqueeze(0)
+            output=resnet(image)
+            _,prediction=torch.max(output,dim=1)
+            print(prediction)
+            if(prediction.item()==1):
+                cv2.putText(frame,"No Mask Detected", (40,40), 2, 2, 255)
+            else: 
+                cv2.putText(frame,"Mask Detected", (40,40), 2, 2, 255)
 
+            return frame 
+def webcam():
+    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer,client_settings=WEBRTC_CLIENT_SETTINGS,)  
 
+def img_upload():
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+    
+        image_tensor = transform(image).float()
+        image_tensor = image_tensor.unsqueeze(0)
+        output=resnet(image_tensor)
+        #st.write(output)
+        _,prediction=torch.max(output,dim=1)
+        if(prediction.item()==1):
+            st.write("No Mask detected")
+                
+        else: 
+            st.write("Mask Detected")
+               
+
+        st.image(image)    
+
+if __name__=="__main__":
+    webcam()
 
 
 
--- pretrained_model.py
+++ pretrained_model.py
@@ -3,14 +3,19 @@ import torch
 import torch.nn as nn
 from collections import OrderedDict
 import torch.nn.functional as F
-vgg16model = models.vgg16_bn(pretrained=True)
-for param in vgg16model.parameters():
+
+resnet = models.resnet18(pretrained=True)
+
+for param in resnet.parameters():
     param.requires_grad = False
-vgg16model.classifier[6] = nn.Sequential(
-    nn.Linear(4096, 2) 
-)
-for param in vgg16model.classifier[6].parameters():
-    param.requires_grad = True
+
+num_ftrs = resnet.fc.in_features
+resnet.fc = nn.Linear(num_ftrs, 2)
+
+total_trainable_params = sum(
+    p.numel() for p in resnet.parameters() if p.requires_grad)
+
+print(total_trainable_params)
 
 
 
